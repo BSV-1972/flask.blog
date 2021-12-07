aff3946c26943341a2ede3e1797c5f42c5fbcebd
BSV-1972 <73275465+BSV-1972@users.noreply.github.com>
2021-12-07 19:41:32 +0300

    'commit_init'

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+
+    return render_template("index.html", title="Главная")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5656)  # стандартный порт у Фласка 5000, у Джанга 8000
